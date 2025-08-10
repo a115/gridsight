b56fb4b399bd9d3cb7386b9d7d5089b942c55f59
Jordan Dimov <jdimov@a115.co.uk>
2025-08-10 21:48:51 +0100

    Adds whitenoise

--- app/settings.py
+++ app/settings.py
@@ -28,6 +28,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() in ("true", "1", "yes")
 ALLOWED_HOSTS = [
     "gridsight.a115.co.uk",
     "gridsight-f9f8.onrender.com",
+    "localhost",
+    "127.0.0.1",
 ]
 
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -131,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
