0d4de57fafa7fe68d7af80bdd71853d9f02cd8eb
Victor Sandoval <vs.software.eng@gmail.com>
2025-08-19 21:20:32 +0100

    chore: add simple Celery app and example task for the live_plants pipeline

---
+++ etl/celery_pipeline/celery.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from app.configuration.celery import celery_settings
+
+
+etl_app = Celery(
+    main="GridSight",
+    broker=celery_settings.broker_url,
+    backend=celery_settings.result_backend,
+)
+
+etl_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    enable_utc=True,
+    broker_connection_retry_on_startup=True,
+)
+
+etl_app.autodiscover_tasks()
--- todo.md
+++ todo.md
@@ -8,9 +8,4 @@ Product:
 3. Plug in some data into the mock **Live Plant Status board** dashboard
 
 Shared app setup/infra:
-
-1. Set up simple settings loading with `pydantic-settings`
-    - Configure Postgres settings
-    - Configure Celery settings
-2. Set up Celery broker container
-3. Set up simple Celery app and add a healthcheck task
+- Add healthcheck Celery task
